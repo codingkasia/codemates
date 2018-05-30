377391c81a6a6b78736718690467eb9699735e10
codingkasia <codingkasia@gmail.com>
2018-05-30 17:01:16 -0400

    models

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
-  get '/', to: "sessions/new"
-  
+  get '/', to: "users#index"
+  get "sessions/new"
+  # resources :locations, only: [:index, :show]
+  # resources :cohorts, only: [:index, :show]
+  resources :users, only: [:show, :new, :create]
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :locations, only: [:index, :show]
-  resources :cohorts, only: [:index, :show]
-  resources :users, only: [:index, :show, :new, :create]
+  
+  
 end
